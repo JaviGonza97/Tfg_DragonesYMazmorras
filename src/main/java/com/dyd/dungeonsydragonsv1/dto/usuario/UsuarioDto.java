2b2170d166291ac785d10970c566a39eca5bfd15
Javier-Gonzalez-jav <javier.gonzalez@javbrain.com>
2025-05-12 10:35:48 +0200

    Actualizacionde fallos que habia en UsuarioDto

--- src/main/java/com/dyd/dungeonsydragonsv1/dto/usuario/UsuarioDto.java
+++ src/main/java/com/dyd/dungeonsydragonsv1/dto/usuario/UsuarioDto.java
@@ -1,4 +1,17 @@
 package com.dyd.dungeonsydragonsv1.dto.usuario;
 
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDto {
+    private Long id;
+    private String username;
+    private String password;
 }
