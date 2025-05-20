2d018e70f55cfd036d11d84a3a7ffed628b0ddeb
Javier-Gonzalez-jav <javier.gonzalez@javbrain.com>
2025-05-20 10:28:27 +0200

    Actualizacion de dtos, y agregacion de repositorios de clase y raza

---
+++ src/main/java/com/dyd/dungeonsydragonsv1/repositorios/ClaseRepository.java
@@ -0,0 +1,7 @@
+package com.dyd.dungeonsydragonsv1.repositorios;
+
+import com.dyd.dungeonsydragonsv1.entidades.Clase;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClaseRepository extends JpaRepository<Clase, Long> {
+}
---
+++ src/main/java/com/dyd/dungeonsydragonsv1/repositorios/RazaRepository.java
@@ -0,0 +1,7 @@
+package com.dyd.dungeonsydragonsv1.repositorios;
+
+import com.dyd.dungeonsydragonsv1.entidades.Raza;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RazaRepository extends JpaRepository<Raza, Long> {
+}
